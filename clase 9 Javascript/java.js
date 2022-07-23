f9c78f780faf88abf353390406c8af0243d7d51b
Dario Lopez <dariolopez@tdf.edu.ar>
2022-07-22 21:02:11 -0300

    Clase Javascript

---
+++ clase 9 Javascript/java.js
@@ -0,0 +1,13 @@
+
+let btn_nombre = document.getElementById("btn_nombre");
+btn_nombre.addEventListener("click",mostrar);
+
+
+function mostrar(){
+   let nombre = "anonimo";
+    let inputNombre =document.getElementById("inputNombre");
+    nombre = inputNombre.value;
+
+    alert("Bienvenido amiguillo  "+ nombre);
+}
+
