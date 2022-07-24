e508d0406c1d5ecb3f6deca581539497606d91fc
Dario Lopez <dariolopez@tdf.edu.ar>
2022-07-24 20:12:00 -0300

    clase javaScript + tarea

--- clase 9 Javascript/java.js
+++ clase 9 Javascript/java.js
@@ -1,13 +1,18 @@
 
-let btn_nombre = document.getElementById("btn_nombre");
-btn_nombre.addEventListener("click",mostrar);
+let btn_entrale = document.getElementById("btn_entrale");
+btn_entrale.addEventListener("click", mostrar);
 
 
-function mostrar(){
-   let nombre = "anonimo";
-    let inputNombre =document.getElementById("inputNombre");
-    nombre = inputNombre.value;
+    function mostrar() {
+        let nombre = "anonimo";
+        let inputNombre =document.getElementById("inputNombre");
+        nombre = inputNombre.value;
+        let apellido = "anonimo";
+        let inputApellido =document.getElementById("inputApellido");
+        apellido = inputApellido.value;
+        let edad = "anonimo";
+        let inputEdad =document.getElementById("inputEdad");
+        edad = inputEdad.value;
 
-    alert("Bienvenido amiguillo  "+ nombre);
-}
-
+        alert("datos del usuario:  "+"  Nombre: "+ nombre+" /  Apellido: "+apellido+" /  Edad " +edad+" años");
+    }
