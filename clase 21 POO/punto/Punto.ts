839da1740b29fe687d38031c4efab10e6c3578bb
Dario-Lopez-TDF <dariolopez@tdf.edu.ar>
2022-09-26 21:09:10 -0300

    tarea Gestor de Libros

---
+++ clase 21 POO/punto/Punto.ts
@@ -0,0 +1,30 @@
+export class Punto {
+    private x: number;
+    private y: number;
+
+    constructor(x:number, y:number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    get_x(): number {
+        return this.x
+    }
+
+    set_x(x:number) {
+        this.x = x;
+    }
+
+    get_y(): number {
+        return this.y
+    }
+
+    set_y(y:number) {
+        this.y = y;
+    }
+
+    toString():string {
+        return '(' + this.x + ' , ' + this.y + ')';
+    }
+
+} // FIN clase Punto
